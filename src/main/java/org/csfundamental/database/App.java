69389adbf1b0334c298b5281e8d981b228c5ebb4
chenliangsjtu@gmail.com <chenliangsjtu@gmail.com>
2023-03-15 01:05:48 -0500

    initial commit

---
+++ src/main/java/org/csfundamental/database/App.java
@@ -0,0 +1,13 @@
+package org.csfundamental.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
