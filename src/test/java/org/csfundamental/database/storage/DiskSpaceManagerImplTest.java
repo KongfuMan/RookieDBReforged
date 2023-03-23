910a533f8848799a1b09ac8da49dd5cb7b61a322
chenliangsjtu@gmail.com <chenliangsjtu@gmail.com>
2023-03-22 23:15:23 -0500

    modified unit test

--- src/test/java/org/csfundamental/database/storage/DiskSpaceManagerImplTest.java
+++ src/test/java/org/csfundamental/database/storage/DiskSpaceManagerImplTest.java
@@ -1,62 +1,31 @@
 package org.csfundamental.database.storage;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 public class DiskSpaceManagerImplTest {
     private static final String DIR = "diskspacemanager";
-    private static final int INIT_PARTS = 3;
     private String dirPath;
-    private File dsmDir;
+    private File dsmRootDir;
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
 
     @Before
-    public void setup() throws IOException {
-        String resourceRoot = getClass().getClassLoader().getResource("").getPath();
-        dsmDir = Paths.get(resourceRoot, DIR).toFile();
-        if (!dsmDir.exists()){
-            dsmDir.mkdirs();
-        }
-        dirPath = dsmDir.getAbsolutePath();
-        for (int i = 0; i < INIT_PARTS; i++){
-            File f = Paths.get(dirPath, String.valueOf(i)).toFile();
-            if (!f.exists()){
-                f.createNewFile();
-            }
-        }
+    public void beforeEach() throws IOException {
+        dsmRootDir = tmpFolder.newFolder(DIR);
+        dirPath = dsmRootDir.getAbsolutePath();
     }
 
-    private void clearPartitionFiles(){
-        if (dsmDir.exists()){
-            for (File file : dsmDir.listFiles()){
-                file.delete();
-            }
-        }
-    }
-
-    @After
-    public void reset_EmptyPartitionFile() throws IOException {
-        clearPartitionFiles();
-        if (dsmDir.exists()){
-            dsmDir.delete();
-        }
-    }
-
-    @Test
-    public void createNewDiskSpaceManager() throws IOException {
-        IDiskSpaceManager dsm = new DiskSpaceManagerImpl(dirPath);
-        int partNum = dsm.allocPart();
-        Assert.assertEquals(3, partNum);
+    private IDiskSpaceManager createNewDiskSpaceManager() {
+        return new DiskSpaceManagerImpl(dirPath);
     }
 
     @Test
     public void allocPartitionsWithMultipleThread() throws InterruptedException {
-        IDiskSpaceManager dsm = new DiskSpaceManagerImpl(dirPath);
+        IDiskSpaceManager dsm = createNewDiskSpaceManager();
         int nThread = 10;
         int allocCount = 30;
         Thread[] allocPartWorkers = new Thread[nThread];
@@ -79,6 +48,6 @@ public class DiskSpaceManagerImplTest {
             worker.join();
         }
 
-        Assert.assertEquals(nThread * allocCount + INIT_PARTS, dsm.getCurrentPartNum());
+        Assert.assertEquals(nThread * allocCount, dsm.getCurrentPartNum());
     }
 }
--- src/test/java/org/csfundamental/database/storage/PartitionTest.java
+++ src/test/java/org/csfundamental/database/storage/PartitionTest.java
@@ -1,11 +1,11 @@
 package org.csfundamental.database.storage;
 
 import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.file.Paths;
 import java.util.UUID;
 
 import static org.csfundamental.database.storage.DiskSpaceManagerImpl.DATA_PAGES_PER_HEADER;
@@ -13,79 +13,52 @@ import static org.csfundamental.database.storage.DiskSpaceManagerImpl.HEADER_PAG
 import static org.csfundamental.database.storage.IDiskSpaceManager.PAGE_SIZE;
 
 public class PartitionTest {
-    private static final String PARTITION_DIR = "partitions";
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
     private static final String EMPTY_PARTITION = "empty_partition";
-
-    private File partDir;
-    private File emptyPart;
-    private String emptyFile;
-
+    private File emptyPartFile;
+    private String emptyPartPath;
     @Before
-    public void setup() throws IOException {
-        String resourceRoot = getClass().getClassLoader().getResource("").getPath();
-        partDir = Paths.get(resourceRoot, PARTITION_DIR).toFile();
-        emptyPart = Paths.get(partDir.getAbsolutePath(), EMPTY_PARTITION).toFile();
-        emptyFile = emptyPart.getAbsolutePath();
-        clearPartitionFiles();
-        if (!partDir.exists()){
-            partDir.mkdirs();
-        }
-        emptyPart.createNewFile();
+    public void beforeEach() throws IOException {
+        emptyPartFile = tmpFolder.newFile(EMPTY_PARTITION);
+        emptyPartPath = emptyPartFile.getAbsolutePath();
     }
-
-    private void clearPartitionFiles(){
-        if (partDir.exists()){
-            for (File file : partDir.listFiles()){
-                file.delete();
-            }
-        }
-    }
-
-    @After
-    public void reset_EmptyPartitionFile() throws IOException {
-        clearPartitionFiles();
-        if (partDir.exists()){
-            partDir.delete();
-        }
-    }
-
     @Test
     public void alloc_DataPages_Under_FirstNHeaderPage() throws Exception {
         Partition part1 = new Partition(0);
-        part1.open(emptyFile);
+        part1.open(emptyPartPath);
         for (int hIdx = 0; hIdx < HEADER_PAGES_PER_MASTER; hIdx += 32){
             for (int dIdx = 0; dIdx < DATA_PAGES_PER_HEADER; dIdx += DATA_PAGES_PER_HEADER - 1){
                 part1.allocPage(hIdx, dIdx);
                 Partition part2 = new Partition(0);
-                part2.open(emptyFile);
+                part2.open(emptyPartPath);
                 comparePartition(part1, part2);
             }
         }
     }
-
     @Test
     public void dealloc_DataPages() throws Exception {
         Partition part1 = new Partition(0);
-        part1.open(emptyFile);
+        part1.open(emptyPartPath);
         int allocCount = 10;
         for (int i = 0; i < allocCount; i++){
             int pageNum = part1.allocPage();
             Assert.assertEquals(pageNum, i);
             Partition part2 = new Partition(0);
-            part2.open(emptyFile);
+            part2.open(emptyPartPath);
             comparePartition(part1, part2);
         }
 
         for (int i = 0; i < allocCount - 1; i++){
             part1.freePage(i);
             Partition part2 = new Partition(0);
-            part2.open(emptyFile);
+            part2.open(emptyPartPath);
             comparePartition(part1, part2);
         }
 
         part1.freePage(allocCount - 1);
         Partition part2 = new Partition(0);
-        part2.open(emptyFile);
+        part2.open(emptyPartPath);
         int[] master = part2.getMasterPage();
         Assert.assertArrayEquals(master, new int[master.length]);
         byte[][] headers = part2.getHeaderPages();
@@ -93,11 +66,10 @@ public class PartitionTest {
             Assert.assertNull(headers[i]);
         }
     }
-
     @Test
     public void free_AllDataPages() throws IOException {
         Partition part1 = new Partition(0);
-        part1.open(emptyFile);
+        part1.open(emptyPartPath);
         int allocCount = 10;
         for (int i = 0; i < allocCount; i++){
             part1.allocPage();
@@ -105,7 +77,7 @@ public class PartitionTest {
         part1.freeDataPages();
 
         Partition part2 = new Partition(0);
-        part2.open(emptyFile);
+        part2.open(emptyPartPath);
         int[] master = part2.getMasterPage();
         Assert.assertArrayEquals(master, new int[master.length]);
         byte[][] headers = part2.getHeaderPages();
@@ -113,27 +85,24 @@ public class PartitionTest {
             Assert.assertNull(headers[i]);
         }
     }
-
     @Test(expected = PageException.class)
     public void writeUnallocatedPage() throws IOException {
         Partition part1 = new Partition(0);
-        part1.open(emptyFile);
+        part1.open(emptyPartPath);
         byte[] buf = new byte[PAGE_SIZE];
         part1.writePage(0, buf);
     }
-
     @Test(expected = PageException.class)
     public void readUnallocatedPage() throws IOException {
         Partition part = new Partition(0);
-        part.open(emptyFile);
+        part.open(emptyPartPath);
         byte[] buf = new byte[PAGE_SIZE];
         part.readPage(0, buf);
     }
-
     @Test
     public void RWAllocatedPage() throws IOException {
         Partition part = new Partition(0);
-        part.open(emptyFile);
+        part.open(emptyPartPath);
         int pageNum = part.allocPage();
         byte[] wBuf = new byte[PAGE_SIZE];
         ByteBuffer buf = ByteBuffer.wrap(wBuf);
@@ -147,7 +116,6 @@ public class PartitionTest {
         part.readPage(pageNum, rBuf);
         Assert.assertArrayEquals(wBuf, rBuf);
     }
-
     private void comparePartition(Partition part1, Partition part2){
         Assert.assertArrayEquals(part1.getMasterPage(), part1.getMasterPage());
         byte[][] headerPages1 = part1.getHeaderPages();
