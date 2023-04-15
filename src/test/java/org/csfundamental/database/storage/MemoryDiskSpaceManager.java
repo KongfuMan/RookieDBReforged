ed79251419b024546ff5176f759e2bd9efe5c771
Charles <charles@Charless-Air-2.lan>
2023-04-15 17:48:03 -0500

    add memory disk manager for unit test

--- src/test/java/org/csfundamental/database/storage/MemoryDiskSpaceManager.java
+++ src/test/java/org/csfundamental/database/storage/MemoryDiskSpaceManager.java
@@ -1,63 +1,121 @@
 package org.csfundamental.database.storage;
 
-import java.io.IOException;
-import java.util.Map;
+import org.csfundamental.database.storage.DiskSpaceManager;
+import org.csfundamental.database.storage.PageException;
 
-public class MemoryDiskSpanceManager implements DiskSpaceManager{
-    private Map<Long, byte[]> dataMap;
+import java.util.*;
+
+/**
+ * "Disk" space manager that really just keeps things in memory. Not thread safe.
+ * For test purpose
+ */
+public class MemoryDiskSpaceManager implements DiskSpaceManager {
+    private Map<Integer, Set<Integer>> partitions = new HashMap<>();
+    private Map<Integer, Integer> nextPageNum = new HashMap<>();
+    private Map<Long, byte[]> pages = new HashMap<>();
+    private int nextPartitionNum = 0;
+
+    @Override
+    public void close() {}
 
     @Override
     public int allocPart() {
-        return 0;
+        partitions.put(nextPartitionNum, new HashSet<>());
+        nextPageNum.put(nextPartitionNum, 0);
+        return nextPartitionNum++;
     }
 
     @Override
     public int allocPart(int partNum) {
-        return 0;
+        if (partitions.containsKey(partNum)) {
+            throw new IllegalStateException("partition " + partNum + " already allocated");
+        }
+        partitions.put(partNum, new HashSet<>());
+        nextPageNum.put(partNum, 0);
+        nextPartitionNum = partNum + 1;
+        return partNum;
     }
 
     @Override
     public void freePart(int partNum) {
-
+        if (!partitions.containsKey(partNum)) {
+            throw new NoSuchElementException("partition " + partNum + " not allocated");
+        }
+        for (Integer pageNum : partitions.remove(partNum)) {
+            pages.remove(DiskSpaceManager.getVirtualPageNum(partNum, pageNum));
+        }
+        nextPageNum.remove(partNum);
     }
 
     @Override
     public long allocPage(int partNum) {
-        return 0;
+        if (!partitions.containsKey(partNum)) {
+            throw new IllegalArgumentException("partition " + partNum + " not allocated");
+        }
+        int ppageNum = nextPageNum.get(partNum);
+        nextPageNum.put(partNum, ppageNum + 1);
+        long pageNum = DiskSpaceManager.getVirtualPageNum(partNum, ppageNum);
+        partitions.get(partNum).add(ppageNum);
+        pages.put(pageNum, new byte[DiskSpaceManager.PAGE_SIZE]);
+        return pageNum;
     }
 
     @Override
     public long allocPage(long page) {
-        return 0;
+        int partNum = DiskSpaceManager.getPartNum(page);
+        int ppageNum = DiskSpaceManager.getPageNum(page);
+        if (!partitions.containsKey(partNum)) {
+            throw new IllegalArgumentException("partition " + partNum + " not allocated");
+        }
+        if (pages.containsKey(page)) {
+            throw new IllegalStateException("page " + page + " already allocated");
+        }
+        nextPageNum.put(partNum, ppageNum + 1);
+        partitions.get(partNum).add(ppageNum);
+        pages.put(page, new byte[DiskSpaceManager.PAGE_SIZE]);
+        return page;
     }
 
     @Override
     public void freePage(long page) {
-
+        if (!pages.containsKey(page)) {
+            throw new NoSuchElementException("page " + page + " not allocated");
+        }
+        int partNum = DiskSpaceManager.getPartNum(page);
+        int pageNum = DiskSpaceManager.getPageNum(page);
+        partitions.get(partNum).remove(pageNum);
+        pages.remove(page);
     }
 
     @Override
-    public void readPage(long page, byte[] data) {
-
+    public void readPage(long page, byte[] buf) {
+        if (buf.length != DiskSpaceManager.PAGE_SIZE) {
+            throw new IllegalArgumentException("bad buffer size");
+        }
+        if (!pages.containsKey(page)) {
+            throw new PageException("page " + page + " not allocated");
+        }
+        System.arraycopy(pages.get(page), 0, buf, 0, DiskSpaceManager.PAGE_SIZE);
     }
 
     @Override
-    public void writePage(long page, byte[] data) {
-
+    public void writePage(long page, byte[] buf) {
+        if (buf.length != DiskSpaceManager.PAGE_SIZE) {
+            throw new IllegalArgumentException("bad buffer size");
+        }
+        if (!pages.containsKey(page)) {
+            throw new PageException("page " + page + " not allocated");
+        }
+        System.arraycopy(buf, 0, pages.get(page), 0, DiskSpaceManager.PAGE_SIZE);
     }
 
     @Override
     public boolean pageAllocated(long page) {
-        return false;
+        return pages.containsKey(page);
     }
 
     @Override
     public int getCurrentPartNum() {
         return 0;
-    }
-
-    @Override
-    public void close() throws IOException {
-
     }
 }
