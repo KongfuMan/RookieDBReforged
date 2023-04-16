34e22cde8bcc35b85b99b987ed3e57bc0d0e7338
Charles <charles@Charless-Air-2.lan>
2023-04-16 02:20:55 -0500

    Finished BufferManager

--- src/main/java/org/csfundamental/database/buffer/BufferManager.java
+++ src/main/java/org/csfundamental/database/buffer/BufferManager.java
@@ -5,11 +5,20 @@ import org.csfundamental.database.storage.PageException;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.BiConsumer;
 
-public class BufferManager implements Closeable {
+/**
+ * Buffer manager follows the STEAL & NO-FORCE policy of ARIES recovery protocol for better performance.
+ * STEAL: the frame can be swapped out before the trx commit.
+ * NO-FORCE: buffer frame updated by the trx will not be flushed immediately after
+ *           trx commit.
+ * */
+public class BufferManager implements AutoCloseable {
     // Reserve 36 bytes on each page to book keep info for recovery
     // (used to store the pageLSN, and to ensure that a redo-only/undo-only log record can
     // fit on one page).
@@ -21,11 +30,14 @@ public class BufferManager implements Closeable {
     private final ReentrantLock managerLock;
     private long numIO;
 
+    private final Set<BufferFrame> inValidFrames;
+
     public BufferManager(DiskSpaceManager diskSpaceManager, int capacity){
         this.diskSpaceManager = diskSpaceManager;
         this.cacheStrategy = new LRUCacheStrategy(capacity);
         this.managerLock = new ReentrantLock();
         this.numIO = 0;
+        this.inValidFrames = new HashSet<>();
     }
 
     /**
@@ -37,45 +49,7 @@ public class BufferManager implements Closeable {
      * @return buffer frame with specified page loaded
      */
     BufferFrame fetchPageFrame(long page) {
-        Frame newFrame;
-        Frame evictedFrame;
-        managerLock.lock();
-        try{
-            if (!diskSpaceManager.pageAllocated(page)){
-                throw new PageException("Page not allocated");
-            }
-            newFrame = (Frame)cacheStrategy.get(page);
-            if (newFrame != null){
-                return newFrame;
-            }
-            byte[] data = new byte[DiskSpaceManager.PAGE_SIZE];
-            newFrame = new Frame(data, page);
-            evictedFrame = (Frame)cacheStrategy.put(page,newFrame);
-            if (evictedFrame == null){
-                return newFrame;
-            }
-            evictedFrame.frameLock.lock();
-        }finally {
-            managerLock.unlock();
-        }
-
-        try{
-            evictedFrame.invalidate();
-        }finally {
-            evictedFrame.frameLock.unlock();
-        }
-
-        newFrame.frameLock.lock();
-        try{
-            newFrame.pin();
-            // read data into buffer frame from disk;
-            diskSpaceManager.readPage(page, newFrame.content);
-            this.incrementNumIO();
-            return newFrame;
-        }finally {
-            newFrame.unpin();
-            newFrame.frameLock.unlock();
-        }
+        return fetchPageFrame(page, false);
     }
 
     /**
@@ -97,12 +71,66 @@ public class BufferManager implements Closeable {
      */
     BufferFrame fetchNewPageFrame(int partNum) {
         long page = diskSpaceManager.allocPage(partNum);
-        return fetchPageFrame(page);
+        return fetchPageFrame(page, true);
     }
 
-    Page fetchNewPage(int partNum) {
+    public Page fetchNewPage(int partNum) {
         long page = diskSpaceManager.allocPage(partNum);
-        return fetchPage(page);
+        return this.frameToPage(this.fetchPageFrame(page, true));
+    }
+
+    /**
+     * Fetches a buffer frame for the specified page.
+     * @param page The virtual page number
+     * @param newAllocated indicating whether the page is newly allocated one, s.t. no load action needed.
+     * @return buffer frame which must be pinned to prevent being swapped out.
+     */
+    private BufferFrame fetchPageFrame(long page, boolean newAllocated) {
+        Frame newFrame;
+        Frame evictedFrame;
+        managerLock.lock();
+        try{
+            if (!diskSpaceManager.pageAllocated(page)){
+                throw new PageException("Page not allocated");
+            }
+            newFrame = (Frame)cacheStrategy.get(page);
+            if (newFrame != null){
+                // cache hit, then just return the frame
+                newFrame.pin();
+                return newFrame;
+            }
+            // cache miss, either because the frame was previously swapped out or this is a newly allocated page
+            byte[] data = new byte[DiskSpaceManager.PAGE_SIZE];
+            newFrame = new Frame(data, page);
+            evictedFrame = (Frame)cacheStrategy.put(page,newFrame);
+        }finally {
+            managerLock.unlock();
+        }
+
+        if (evictedFrame != null){
+            evictedFrame.frameLock.lock();
+            try{
+                evictedFrame.invalidate();
+                inValidFrames.add(evictedFrame);
+            }finally {
+                evictedFrame.frameLock.unlock();
+            }
+        }
+
+        newFrame.frameLock.lock();
+        try{
+            newFrame.pin();
+            if (newAllocated){
+                // newly allocated page on-disk, so no load action needed.
+                return newFrame;
+            }
+            // read data into buffer frame from disk;
+            diskSpaceManager.readPage(page, newFrame.content);
+            this.incrementNumIO();
+            return newFrame;
+        }finally {
+            newFrame.frameLock.unlock();
+        }
     }
 
     /**
@@ -113,19 +141,34 @@ public class BufferManager implements Closeable {
      * @param page page to free
      */
     public void freePage(Page page) {
+        freePage(page.getPageNum());
+    }
+
+    private void freePage(long pageNum){
         managerLock.lock();
-        long pageNum = page.getPageNum();
-        BufferFrame frame = cacheStrategy.get(pageNum);
-        frame.flush();
-        cacheStrategy.remove(pageNum);
-        managerLock.unlock();
+        try{
+            BufferFrame frame = cacheStrategy.get(pageNum);
+            frame.flush();
+            frame.invalidate(); //should not allow any action after deallocate
+            cacheStrategy.remove(pageNum);
+            diskSpaceManager.freePage(pageNum);
+        }finally {
+            managerLock.unlock();
+        }
+
     }
 
     public void freePart(int partNum) {
         managerLock.lock();
         try{
-            //TODO: evict all pages in this partition
-
+            // TODO: evict all pages in this partition
+            Iterator<BufferFrame> itr = cacheStrategy.getAllPageFrames().iterator();
+            while(itr.hasNext()){
+                BufferFrame frame = itr.next();
+                if (DiskSpaceManager.getPartNum(frame.getPageNum()) == partNum){
+                    this.freePage(frame.getPageNum());
+                }
+            }
             diskSpaceManager.freePart(partNum);
         }finally {
             managerLock.unlock();
@@ -155,21 +198,18 @@ public class BufferManager implements Closeable {
 
     }
 
+    /**
+     * Private method. Called should guarantee thread-safety
+     * */
     private void incrementNumIO(){
-        managerLock.lock();
         ++numIO;
-        managerLock.unlock();
     }
 
-    public long getNumIOs() {
-        long res = 0;
-        managerLock.lock();
-        res = numIO;
-        managerLock.unlock();
-        return res;
+    long getNumIOs() {
+        return numIO;
     }
 
-    public void close() throws IOException {
+    public void close() {
 
     }
 
@@ -183,13 +223,15 @@ public class BufferManager implements Closeable {
     }
 
     class Frame extends BufferFrame {
-        private final byte[] content;
+        private byte[] content;
         private final long page;
         final ReentrantLock frameLock;
         private boolean dirty;
+        private boolean logPage;
 
         /**
-         * Mark if the buffer frame is evicted.
+         * Mark if the buffer frame is going to be reclaimed.
+         * A reclaim worker will later reclaim invalid frame whose pinCount == 0.
          * */
         private boolean isValid;
 
@@ -202,6 +244,7 @@ public class BufferManager implements Closeable {
             this.dirty = false;
             this.frameLock = new ReentrantLock();
             this.isValid = true;
+            this.logPage = false;
         }
 
         @Override
@@ -214,7 +257,12 @@ public class BufferManager implements Closeable {
             frameLock.lock();
             pin();
             try {
+                if (!this.dirty){
+                    return;
+                }
                 BufferManager.this.diskSpaceManager.writePage(this.page, this.content);
+                BufferManager.this.incrementNumIO();
+                this.dirty = false;
             }finally {
                 unpin();
                 frameLock.unlock();
@@ -224,26 +272,57 @@ public class BufferManager implements Closeable {
         @Override
         void readBytes(short position, short len, byte[] buf) {
             frameLock.lock();
-            pin();
-            System.arraycopy(content, position, buf, 0, len);
-            unpin();
-            frameLock.unlock();
+            try{
+                if (!this.isValid()){
+                    throw new IllegalStateException("Reading from invalid buffer frame");
+                }
+                pin();
+                System.arraycopy(content, position + dataOffset(), buf, 0, len);
+            }finally {
+                unpin();
+                frameLock.unlock();
+            }
         }
 
         @Override
         void writeBytes(short position, short len, byte[] buf) {
             frameLock.lock();
-            pin();
-            // TODO: transaction atomicity and durability
-            System.arraycopy(buf, 0, content, position, len);
-            dirty = true;
-            unpin();
-            frameLock.unlock();
+            try{
+                if (!this.isValid()){
+                    throw new IllegalStateException("Writing to invalid buffer frame");
+                }
+                pin();
+                // TODO: transaction atomicity and durability
+
+                System.arraycopy(buf, 0, content, position + dataOffset(), len);
+                this.dirty = true;
+            }finally {
+                unpin();
+                frameLock.unlock();
+            }
         }
 
+        private short dataOffset() {
+            if (logPage) {
+                return 0;
+            } else {
+                return BufferManager.RESERVED_SPACE;
+            }
+        }
+
+        /**
+         * The frame is marked invalid if it is going to be reclaimed from memory.
+         * As a result, no action is allowed except for unpin.
+         * */
         @Override
         void invalidate(){
+            if (this.isValid()){
+                // supposed to be a worker thread to flush. Do it now to make test easier.
+                this.flush();
+            }
             this.isValid = false;
+            // make it GCed
+            this.content = null;
         }
 
         @Override
--- src/test/java/org/csfundamental/database/buffer/BufferManagerTest.java
+++ src/test/java/org/csfundamental/database/buffer/BufferManagerTest.java
@@ -1,4 +1,291 @@
+package org.csfundamental.database.buffer;
+
+import org.csfundamental.database.storage.DiskSpaceManager;
+import org.csfundamental.database.storage.MemoryDiskSpaceManager;
+import org.csfundamental.database.storage.PageException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static org.csfundamental.database.storage.DiskSpaceManager.PAGE_SIZE;
 import static org.junit.Assert.*;
+
 public class BufferManagerTest {
-  
+    private DiskSpaceManager diskSpaceManager;
+    private BufferManager bufferManager;
+
+    private byte[] generateRandomByteArray(){
+        byte[] data = new byte[PAGE_SIZE];
+        Random rand = new Random();
+        rand.nextBytes(data);
+        return data;
+    }
+
+    @Before
+    public void beforeEach() {
+        diskSpaceManager = new MemoryDiskSpaceManager();
+        bufferManager = new BufferManager(diskSpaceManager, 5);
+    }
+
+    @After
+    public void afterEach() {
+        bufferManager.close();
+        diskSpaceManager.close();
+    }
+
+    @Test
+    public void validateMemoryDiskSpaceManager(){
+        diskSpaceManager.allocPart(0);
+        byte[] rData = new byte[PAGE_SIZE];
+        for (long i = 0L; i < 10L; i++){
+            byte[] wData = generateRandomByteArray();
+            diskSpaceManager.allocPage(i);
+            diskSpaceManager.writePage(i, wData);
+            Arrays.fill(rData, (byte)0);
+            diskSpaceManager.readPage(i, rData);
+            Assert.assertArrayEquals(wData, rData);
+        }
+    }
+
+    @Test
+    public void testFetchNewPage() {
+        int partNum = diskSpaceManager.allocPart(1);
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame2 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame3 = bufferManager.fetchNewPageFrame(partNum);
+
+        frame1.unpin();
+        frame2.unpin();
+        frame3.unpin();
+
+        Assert.assertTrue(frame1.isValid());
+        Assert.assertTrue(frame2.isValid());
+        Assert.assertTrue(frame3.isValid());
+
+        BufferFrame frame4 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame5 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame6 = bufferManager.fetchNewPageFrame(partNum);
+        frame4.unpin();
+        frame5.unpin();
+        frame6.unpin();
+
+        assertFalse(frame1.isValid());
+        assertTrue(frame2.isValid());
+        assertTrue(frame3.isValid());
+        assertTrue(frame4.isValid());
+        assertTrue(frame5.isValid());
+        assertTrue(frame6.isValid());
+    }
+
+    @Test
+    public void testFetchPage(){
+        int partNum = diskSpaceManager.allocPart(1);
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame2 = bufferManager.fetchPageFrame(frame1.getPageNum());
+
+        frame1.unpin();
+        frame2.unpin();
+
+        Assert.assertSame(frame1, frame2);
+    }
+
+    @Test
+    public void testReadWrite(){
+        int partNum = diskSpaceManager.allocPart(1);
+
+        byte[] expected = new byte[] { (byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF };
+        byte[] actual = new byte[4];
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum);
+        frame1.writeBytes((short) 67, (short) 4, expected);
+        frame1.readBytes((short) 67, (short) 4, actual);
+        frame1.unpin();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFlush(){
+        int partNum = diskSpaceManager.allocPart(1);
+
+        byte[] expected = new byte[] { (byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF };
+        byte[] actual = new byte[DiskSpaceManager.PAGE_SIZE];
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum);
+        frame1.writeBytes((short) 67, (short) 4, expected);
+        frame1.unpin();
+
+        // not flushed yet
+        diskSpaceManager.readPage(frame1.getPageNum(), actual);
+        assertArrayEquals(new byte[4], Arrays.copyOfRange(actual, 67 + BufferManager.RESERVED_SPACE,
+                71 + BufferManager.RESERVED_SPACE));
+
+        // explicitly flushed
+        frame1.flush();
+        diskSpaceManager.readPage(frame1.getPageNum(), actual);
+        assertArrayEquals(expected, Arrays.copyOfRange(actual, 67 + BufferManager.RESERVED_SPACE,
+                71 + BufferManager.RESERVED_SPACE));
+
+        frame1.pin();
+        frame1.writeBytes((short) 33, (short) 4, expected);
+        frame1.unpin();
+        diskSpaceManager.readPage(frame1.getPageNum(), actual);
+
+        assertArrayEquals(new byte[4], Arrays.copyOfRange(actual, 33 + BufferManager.RESERVED_SPACE,
+                37 + BufferManager.RESERVED_SPACE));
+
+        // force an eviction by reaching cache capacity: 5
+        BufferFrame frame2 = bufferManager.fetchNewPageFrame(partNum);
+        frame2.unpin();
+        BufferFrame frame3 = bufferManager.fetchNewPageFrame(partNum);
+        frame3.unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+
+        diskSpaceManager.readPage(frame1.getPageNum(), actual);
+        assertFalse(frame1.isValid());
+        assertArrayEquals(expected, Arrays.copyOfRange(actual, 33 + BufferManager.RESERVED_SPACE,
+                37 + BufferManager.RESERVED_SPACE));
+
+        // non-dirty frame2
+        diskSpaceManager.readPage(frame2.getPageNum(), actual);
+        assertArrayEquals(new byte[4], Arrays.copyOfRange(actual, 33 + BufferManager.RESERVED_SPACE,
+                37 + BufferManager.RESERVED_SPACE));
+    }
+
+    @Test
+    public void testReload(){
+        int partNum = diskSpaceManager.allocPart(1);
+
+        byte[] expected = new byte[] { (byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF };
+        byte[] actual = new byte[4];
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum);
+        frame1.writeBytes((short) 67, (short) 4, expected);
+        frame1.unpin();
+
+        // force a eviction
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+        bufferManager.fetchNewPageFrame(partNum).unpin();
+
+        assertFalse(frame1.isValid());
+
+        // reload page
+        frame1 = bufferManager.fetchPageFrame(frame1.getPageNum());
+        frame1.readBytes((short) 67, (short) 4, actual);
+        frame1.unpin();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFreePage() {
+        int partNum = diskSpaceManager.allocPart(1);
+
+        byte[] expected = new byte[] { (byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF };
+        byte[] actual = new byte[4];
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame2 = bufferManager.fetchNewPageFrame(partNum);
+        Page page3 = bufferManager.fetchNewPage(partNum);
+        BufferFrame frame4 = bufferManager.fetchNewPageFrame(partNum);
+        BufferFrame frame5 = bufferManager.fetchNewPageFrame(partNum);
+
+        frame1.unpin();
+        frame2.unpin();
+        frame4.unpin();
+        frame5.unpin();
+
+        bufferManager.freePage(page3);
+
+        try {
+            diskSpaceManager.readPage(page3.getPageNum(), new byte[DiskSpaceManager.PAGE_SIZE]);
+            fail();
+        } catch (PageException e) { /* do nothing */ }
+
+        BufferFrame frame6  = bufferManager.fetchNewPageFrame(partNum);
+        frame6.unpin();
+        assertTrue(frame1.isValid());
+        assertTrue(frame2.isValid());
+        assertTrue(frame4.isValid());
+        assertTrue(frame5.isValid());
+        assertTrue(frame6.isValid());
+    }
+
+    @Test
+    public void freePart() {
+        int partNum1 = diskSpaceManager.allocPart(1);
+        int partNum2 = diskSpaceManager.allocPart(2);
+
+        BufferFrame frame1 = bufferManager.fetchNewPageFrame(partNum1);
+        BufferFrame frame2 = bufferManager.fetchNewPageFrame(partNum2);
+        BufferFrame frame3 = bufferManager.fetchNewPageFrame(partNum1);
+        BufferFrame frame4 = bufferManager.fetchNewPageFrame(partNum2);
+        BufferFrame frame5 = bufferManager.fetchNewPageFrame(partNum2);
+
+        frame1.unpin();
+        frame2.unpin();
+        frame3.unpin();
+        frame4.unpin();
+        frame5.unpin();
+
+        bufferManager.freePart(partNum1);
+
+        try {
+            diskSpaceManager.readPage(frame1.getPageNum(), new byte[DiskSpaceManager.PAGE_SIZE]);
+            fail();
+        } catch (Exception e) { /* do nothing */ }
+        try {
+            diskSpaceManager.readPage(frame3.getPageNum(), new byte[DiskSpaceManager.PAGE_SIZE]);
+            fail();
+        } catch (Exception e) { /* do nothing */ }
+        try {
+            diskSpaceManager.allocPage(partNum1);
+            fail();
+        } catch (Exception e) { /* do nothing */ }
+
+        BufferFrame frame6  = bufferManager.fetchNewPageFrame(partNum2);
+        BufferFrame frame7  = bufferManager.fetchNewPageFrame(partNum2);
+        frame6.unpin();
+        frame7.unpin();
+        assertFalse(frame1.isValid());
+        assertTrue(frame2.isValid());
+        assertFalse(frame3.isValid());
+        assertTrue(frame4.isValid());
+        assertTrue(frame5.isValid());
+        assertTrue(frame6.isValid());
+        assertTrue(frame7.isValid());
+    }
+
+    @Test(expected = PageException.class)
+    public void testMissingPart() {
+        bufferManager.fetchPageFrame(DiskSpaceManager.getVirtualPageNum(0, 0));
+    }
+
+    @Test(expected = PageException.class)
+    public void testMissingPage() {
+        int partNum = diskSpaceManager.allocPart(1);
+        bufferManager.fetchPageFrame(DiskSpaceManager.getVirtualPageNum(partNum, 0));
+    }
 }
